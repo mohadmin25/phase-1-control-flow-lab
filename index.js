49c57cbff18e534d686cb0ec8d031792cd7bc2cd
mohadmin25 <abuhamar008@gmail.com>
2022-08-17 14:18:27 +0300

    completed

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(customers){
   // Write your code here!
+ if (customers <= 400){
+  return "This one is on me!"
+}else if ( customers>=2000 && customers <=2499){
+  return "I will gladly take your thirty bucks."
+}else {
+  return 'No can do.'
+}
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+ return city === 'NYC' ? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip){
+    case "generous":
+      return 'Thank you so much.'
+      break;
+      case "not as generous":
+        return 'Thank you.';
+        break;
+      default:
+        return 'Bye.'
+  }
 }
